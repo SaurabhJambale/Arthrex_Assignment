db5a15ce4b786165348500d7bb64ba0765ae28c2
Saurabh <saurabhjambale2001@gmail.com>
2024-04-16 09:03:12 +0530

    commit

---
+++ App.py
@@ -0,0 +1,25 @@
+
+from LoggerModule import *
+
+
+
+My_logger_obj=MyLogger()
+
+
+def add (a,b):
+    try:
+        c=a+b
+        My_logger_obj.logger.info(f'Adding {a} and {b}')
+
+    except Exception as e:
+        My_logger_obj.logger.exception(e)
+
+def divide(a,b):
+    try:
+        c=a/b
+        My_logger_obj.logger.info(f'Dividing {a} by {b}')
+    except Exception as e:
+        My_logger_obj.logger.exception(e)
+
+divide(5,7)
+add(4,8)
